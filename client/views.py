b4b3fb167c48a926d8d6deb11670610495047303
Chun Fai <ashchunfai@gmail.com>
2022-11-14 11:06:49 +0800

    firebase db -> postgresql db

--- client/views.py
+++ client/views.py
@@ -1,25 +1,19 @@
-from django.shortcuts import HttpResponse, render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
 from .functions import password_check
 from .models import Customer
-import pyrebase
-
-config = {
-    'apiKey': 'AIzaSyCWI61KjoayJaYBabohGqxZTBztuv-Nmc4',
-    'authDomain': 'odyera-ee197.firebaseapp.com',
-    'databaseURL': 'https://odyera-ee197-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    'projectId': 'odyera-ee197',
-    'storageBucket': 'odyera-ee197.appspot.com',
-    'messagingSenderId': '97850537863',
-    'appId': '1:97850537863:web:78aa399ca115cf6c3bc522',
-    'measurementId': 'G-LQ14FN9B7'
-}
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-database = firebase.database()
 
 
 def index(request):
+    data = {}
+
+    # If a customer is already logged in, retrieve the customer details
+    if 'uid' in request.session:
+        uid = request.session['uid']
+        customer = Customer.objects.get(id=uid)
+        data['customer'] = customer
+        return render(request, 'client/index.html', data)
+
     return render(request, 'client/index.html')
 
 
@@ -40,36 +34,31 @@ def register(request):
             'last_name': last_name,
             'email_address': email_address
         }
-        errorsDict = {}
+        errors = {}
 
         # Validates password using custom regex checking function
         password_validation_errors = password_check(password)
         if len(password_validation_errors) != 0:
-            errorsDict['password_validation_errors'] = password_validation_errors
+            errors['password_validation_errors'] = password_validation_errors
 
         # Compares whether password and password confirmation match
         if password != password_confirmation:
-            errorsDict['password_confirmation_error'] = 'Password and password confirmation do not match.'
+            errors['password_confirmation_error'] = 'Password and password confirmation do not match.'
+
+        # Checks whether email address already exists in the database
+        if Customer.objects.filter(email=email_address).exists():
+            errors['existing_email_error'] = 'Email already exists. Please try another one.'
 
         # If there are error messages, re-renders the page with the already filled in user account details and error messages
-        if errorsDict:
-            data['errors'] = errorsDict
+        if errors:
+            data['errors'] = errors
             return render(request, 'client/register.html', data)
 
-        # Try registering a new user account into Firebase Authentication
-        try:
-            user = auth.create_user_with_email_and_password(
-                email_address, password)
-            uid = user['localId']
-            database.child('customers').child(uid).set(data)
-            Customer.objects.create(
-                first_name=first_name, last_name=last_name, email=email_address, password=password)
-            return redirect('/login')
-        # Fails if the email entered already exists in the Firebase Authentication database
-        except:
-            errorsDict['existing_email_error'] = 'Email already exists. Please try another one.'
-            data['errors'] = errorsDict
-            return render(request, 'client/register.html', data)
+        # Register a new customer account into the database
+        hashed_password = make_password(password)
+        Customer.objects.create(
+            first_name=first_name, last_name=last_name, email=email_address, password=hashed_password)
+        return redirect('/login')
     # Everything else goes through here, which only renders the page and nothing else
     else:
         return render(request, 'client/register.html')
@@ -88,20 +77,20 @@ def login(request):
             'email_address': email_address
         }
 
-        # Try logging into the user account by comparing account details from Firebase Authentication
         try:
-            user = auth.sign_in_with_email_and_password(
-                email_address, password)
-        # Fails if no email address and/or password match with the stored accounts in Firebase Authentication
-        except:
+            customer = Customer.objects.get(email=email_address)
+            # Checks if the hashed password in the database matches the entered password
+            is_matched = check_password(password, customer.password)
+            if not is_matched:
+                data['invalid_credentials_error'] = 'Invalid email or password. Please try again.'
+                return render(request, 'client/login.html', data)
+            else:
+                uid = customer.id
+                request.session['uid'] = uid
+                return redirect('/')
+        except Customer.DoesNotExist:
             data['invalid_credentials_error'] = 'Invalid email or password. Please try again.'
             return render(request, 'client/login.html', data)
-        # Stores logged in user account details into current session
-        uid = user['localId']
-        user = database.child('customers').child(uid).get().val()
-        request.session['uid'] = str(uid)
-        request.session['user'] = user
-        return redirect('/')
     # Everything else goes through here, which only renders the page and nothing else
     else:
         return render(request, 'client/login.html')
@@ -110,7 +99,6 @@ def login(request):
 def logout(request):
     try:
         del request.session['uid']
-        del request.session['user']
     except:
         pass
     return redirect('/login')
@@ -130,17 +118,17 @@ def account(request):
         email_address = request.POST.get('email_address')
         password = request.POST.get('password')
         password_confirmation = request.POST.get('password_confirmation')
-        errorsDict = {}
+        errors = {}
 
         # Compares whether password and password confirmation match
         if password != password_confirmation:
-            errorsDict['password_confirmation_error'] = 'Password and password confirmation do not match.'
+            errors['password_confirmation_error'] = 'Password and password confirmation do not match.'
 
         try:
             user = auth.sign_in_with_email_and_password(
                 old_email_address, password)
         except:
-            errorsDict['invalid_credentials_error'] = 'Incorrect password. Please try again.'
+            errors['invalid_credentials_error'] = 'Incorrect password. Please try again.'
             return render(request, 'client/account.html')
     # Everything else goes through here, which only renders the page and nothing else
     else:
