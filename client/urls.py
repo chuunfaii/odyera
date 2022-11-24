471db108e039cbcaa8c9b490675c4839c5fa54ff
Chun Fai <ashchunfai@gmail.com>
2022-11-24 10:58:47 +0800

    modify payment url

--- client/urls.py
+++ client/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
     path('restaurant/<int:id>', views.restaurant, name='restaurant'),
     path('restaurant/<int:id>/menu', views.menu, name='menu'),
     path('restaurant/<int:id>/order', views.order, name='order'),
-    path('payment', views.payment, name='payment'),
+    path('restaurant/<int:id>/payment', views.payment, name='payment'),
     path('foodTrend_particular', views.foodTrend_particular,
          name='foodTrend_particular'),
     path('foodTrend_whole', views.foodTrend_whole, name='foodTrend_whole'),
     path('order_history', views.order_history, name='order_history'),
-    path('report_whole', views.report_whole, name = 'report_whole'),
-    path('report_particular', views.report_particular, name = 'report_particular'),
+    path('report_whole', views.report_whole, name='report_whole'),
+    path('report_particular', views.report_particular, name='report_particular'),
 
 ]
