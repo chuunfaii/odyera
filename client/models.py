da6954c7019b63c1455634f9735654ae0dd3c2fe
Chun Fai <ashchunfai@gmail.com>
2022-12-01 23:10:07 +0800

    add model SentimentAnalysis

--- client/models.py
+++ client/models.py
@@ -63,3 +63,10 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     date_time = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+class SentimentAnalysis(models.Model):
+    polarity_score = models.IntegerField()
+    compound_score = models.IntegerField()
+    super_score = models.IntegerField()
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
