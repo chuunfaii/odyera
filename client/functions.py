57ccc6219aa856708eb4eddae7312f1a321a5a70
Chun Fai <ashchunfai@gmail.com>
2022-12-01 23:17:03 +0800

    done calc compound score func

--- client/functions.py
+++ client/functions.py
@@ -1,22 +1,36 @@
 from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from client.models import Review, SentimentAnalysis
 
 
 def calculate_super_score_all():
     reviews = Review.objects.all()
     polarity_scores = []
+    compound_scores = []
 
     for review in reviews:
         polarity_score = calculate_polarity_score(review)
+        compound_score = calculate_compound_score(review)
+
         polarity_scores.append(polarity_score)
+        compound_scores.append(compound_score)
 
     print(polarity_scores)
+    print(compound_scores)
 
 
 def calculate_polarity_score(review):
     res = TextBlob(review.text)
     polarity_score = res.sentiment.polarity
     return round(polarity_score)
+
+
+def calculate_compound_score(review):
+    sia = SentimentIntensityAnalyzer()
+    polarity_scores = sia.polarity_scores(review.text)
+    compound_score = polarity_scores['compound']
+    print(compound_score)
+    return round(compound_score)
 
 
 def password_check(password):
